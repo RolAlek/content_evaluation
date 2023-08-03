32e0bcc3bc39b8addfbaf6b6215452aa53b678e4
Aleksandr Rol <rolaleksandr75@gmail.com>
2023-08-03 19:11:08 +0900

    dam...

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,7 +11,7 @@ class IsAuthorOrStaff(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        elif request.user.is_authenticated:
+        if request.user.is_authenticated:
             return (
                 request.user == obj.author
                 or request.user.is_superuser
